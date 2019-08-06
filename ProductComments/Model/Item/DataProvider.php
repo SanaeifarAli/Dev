8407b5dc37968336e2665105fe1fac95473e2af1
ali sanaeifar <alisanaeifar@yahoo.com>
2019-08-06 19:25:53 +0430

    debug error of compilation of dataprovider

--- ProductComments/Model/Item/DataProvider.php
+++ ProductComments/Model/Item/DataProvider.php
@@ -14,9 +14,9 @@ class DataProvider extends AbstractDataProvider
     /**
      * DataProvider constructor.
      *
-     * @param $name
-     * @param $primaryFieldName
-     * @param $requestFieldName
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
      * @param CollectionFactory      $contactCollectionFactory
      * @param DataPersistorInterface $dataPersistor
      * @param array                  $meta
